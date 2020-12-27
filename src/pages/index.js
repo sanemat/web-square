4ad82c07f3fb52760915091b15fab8a43650bbaf
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-27 21:40:48 +0900

    refactor(image): named export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Image from "../components/image"
 import SEO from "../components/seo"
 import { Layout } from "../components/layout"
+import { Image } from "../components/image"
 
 const IndexPage = () => (
   <Layout>
