229a6b756f84d0f7394c9f6cd5fb12464bed875a
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-29 22:51:19 +0900

    refactor(component): extract component/tic-tac-toe

---
+++ src/components/tic-tac-toe/game.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Board } from "./board"
+import "./tic-tac-toe.css"
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export { Game }
