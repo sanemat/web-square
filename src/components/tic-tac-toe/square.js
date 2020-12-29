ff64f0359cfa79e54a998f09c30791bfe3b319b9
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-29 23:08:48 +0900

    chore(square): set state

--- src/components/tic-tac-toe/square.js
+++ src/components/tic-tac-toe/square.js
@@ -1,15 +1,22 @@
 import React from "react"
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          alert("click")
+          this.setState({ value: "X" })
         }}
       >
-        {this.props.value}
+        {this.state.value}
       </button>
     )
   }
