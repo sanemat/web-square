7e9575191e7e5cbe294802539736b6b6a5d6ae5e
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-29 23:32:41 +0900

    chore(square): functional component

--- src/components/tic-tac-toe/square.js
+++ src/components/tic-tac-toe/square.js
@@ -1,18 +1,9 @@
 import React from "react"
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick()
-        }}
-      >
-        {this.props.value}
-      </button>
-    )
-  }
+export function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
-
-export { Square }
