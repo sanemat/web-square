3f85b75d54ae6772548f9c75e43462aa2ca56a8c
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-29 23:25:13 +0900

    chore(board): baord has state

--- src/components/tic-tac-toe/board.js
+++ src/components/tic-tac-toe/board.js
@@ -2,8 +2,20 @@ import React from "react"
 import { Square } from "./square"
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
@@ -29,6 +41,12 @@ class Board extends React.Component {
         </div>
       </div>
     )
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = "X"
+    this.setState({ squares: squares })
   }
 }
 
