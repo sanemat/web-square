3795c428e6c3224c0eaa85e08f8acf7665660300
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-12-30 01:06:05 +0900

    chore(board): taking turns

--- src/components/tic-tac-toe/board.js
+++ src/components/tic-tac-toe/board.js
@@ -6,6 +6,7 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
@@ -19,7 +20,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X"
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`
 
     return (
       <div>
@@ -45,8 +46,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = "X"
-    this.setState({ squares: squares })
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 }
 
