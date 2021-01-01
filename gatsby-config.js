271d04e2d1afc053ea8327c4a0631190c2c70a40
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-01-01 23:22:08 +0900

    chore(sitemeta): update site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Square supporter`,
+    description: `support drawing qrcode`,
+    author: `@sanemat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          Matt
         </footer>
       </div>
     </>
