a6f2eb438ec274c9b8c72580736f234f83743784
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-01-02 00:02:04 +0900

    feat(worktable): add worktable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Layout } from "../components/layout"
+import { Worktable } from "../components/square/worktable"
 
 export default function IndexPage() {
   return (
     <Layout>
-      <>index tsx</>
+      <Worktable></Worktable>
     </Layout>
   )
 }
