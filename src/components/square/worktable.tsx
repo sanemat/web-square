aaa19ae651153d689476954dc0008f085c25d92d
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-01-02 22:08:47 +0900

    feat(square): value

--- src/components/square/worktable.tsx
+++ src/components/square/worktable.tsx
@@ -1,5 +1,34 @@
 import React from "react"
 
 export function Worktable() {
-  return <>worktable</>
+  return (
+    <div className="worktable">
+      <div className="worktable-board">
+        <Board></Board>
+      </div>
+    </div>
+  )
+}
+
+function Board({ squares }) {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="0" />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+        <Square value="7" />
+      </div>
+    </>
+  )
+}
+
+function Square({ value }) {
+  return <button className="square">{value}</button>
 }
